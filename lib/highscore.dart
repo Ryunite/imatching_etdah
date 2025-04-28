7dc10490b9bd6bbfa1ec65cbb187a856c04fbf6c
Ryunite <lazyryu12@gmail.com>
2025-04-28 18:07:31 +0700

    Create highscore.dart

---
+++ lib/highscore.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HighScore extends StatefulWidget {
+  @override
+  _HighScoreState createState() => _HighScoreState();
+}
+
+class _HighScoreState extends State<HighScore> {
+  String _topUser = "No user";
+  int _topPoint = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHighScore();
+  }
+
+  Future<void> _loadHighScore() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _topUser = prefs.getString('top_user') ?? "No user";
+      _topPoint = prefs.getInt('top_point') ?? 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("High Score")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Top User: $_topUser", style: TextStyle(fontSize: 24)),
+            SizedBox(height: 10),
+            Text("Top Score: $_topPoint", style: TextStyle(fontSize: 24)),
+          ],
+        ),
+      ),
+    );
+  }
+}
