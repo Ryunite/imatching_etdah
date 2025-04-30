85037625758ea0595a755ae8995f8bd4cecf154c
Ryunite <lazyryu12@gmail.com>
2025-04-30 13:53:21 +0700

    Update highscore.dart

--- lib/highscore.dart
+++ lib/highscore.dart
@@ -21,25 +21,57 @@ class _HighScoreState extends State<HighScore> {
 
   Future<void> _loadHighScore() async {
     final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _topUser = prefs.getString('top_user') ?? "No user";
-      _topPoint = prefs.getInt('top_point') ?? 0;
-    });
+    List<String> highScores = prefs.getStringList('high_scores') ?? [];
+    // setState(() {
+    //   _topUser = prefs.getString('top_user') ?? "No user";
+    //   _topPoint = prefs.getInt('top_point') ?? 0;
+
+    // });
   }
 
+  Future<List<String>> getHighScores() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList('high_scores') ?? [];
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(title: Text("High Score")),
+  //     body: Center(
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: [
+  //           Text("Top User: $_topUser", style: TextStyle(fontSize: 24)),
+  //           SizedBox(height: 10),
+  //           Text("Top Score: $_topPoint", style: TextStyle(fontSize: 24)),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("High Score")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Top User: $_topUser", style: TextStyle(fontSize: 24)),
-            SizedBox(height: 10),
-            Text("Top Score: $_topPoint", style: TextStyle(fontSize: 24)),
-          ],
-        ),
+      appBar: AppBar(title: Text("Layar High Scores")),
+      body: FutureBuilder<List<String>>(
+        future: getHighScores(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text("Tidak ada highscore yang tersedia."));
+          }
+
+          final scores = snapshot.data!;
+          return ListView.builder(
+            itemCount: scores.length,
+            itemBuilder: (context, index) {
+              final parts = scores[index].split(':');
+              return ListTile(title: Text(parts[0]), trailing: Text(parts[1]));
+            },
+          );
+        },
       ),
     );
   }
