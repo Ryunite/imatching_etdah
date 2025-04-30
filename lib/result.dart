1ed89ee2777e34b35918db4a329aff0dc029cf73
Ryunite <lazyryu12@gmail.com>
2025-04-30 13:53:30 +0700

    Update result.dart

--- lib/result.dart
+++ lib/result.dart
@@ -10,6 +10,23 @@ class Result extends StatefulWidget {
 }
 
 class _ResultState extends State<Result> {
+  int playerPoint = 0;
+  String playerName = "No User";
+
+  @override
+  void initState() {
+    super.initState();
+    loadScore();
+  }
+
+Future<void> loadScore() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      playerName = prefs.getString('user_id') ?? "No user";
+      playerPoint = prefs.getInt('user_point') ?? 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +35,10 @@ class _ResultState extends State<Result> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Point anda ...'),
+            Text('$playerName'),
+            Divider(height: 5, color: Colors.transparent),
+            Text('Point anda $playerPoint',),
+            Divider(height: 10, color: Colors.transparent),
             TextButton(
               onPressed: () {
                 setState(() {
